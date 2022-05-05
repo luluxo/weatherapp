0b127488b9acea687764b9e46822416442d816e9
Luna <cookbark@gmail.com>
2022-05-04 20:37:32 -0500

    Finished index.html, styles.css and index.js

--- index.js
+++ index.js
@@ -21,13 +21,17 @@ let weather = {
            "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").textContent = description;
         document.querySelector(".temp").textContent = temp + "°F";
-        document.querySelector(".humidity").textContent = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").textContent = "Wind speed: " + speed + " mph";
+        document.querySelector(".humidity").textContent =  
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").textContent = 
+            "Wind speed: " + speed + " mph";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +"')"
     },
     search:function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
     }
-};
+}; 
 
 document
 .querySelector(".search button")
@@ -39,4 +43,5 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
     if (event.key == "Enter") {
         weather.search();
     }
-})
+});
+
